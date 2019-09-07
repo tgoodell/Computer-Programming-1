30337c4626fdcbab73a462b2da866c15b098d5ff
Tristan Goodell <tgoodell@disroot.org>
2019-09-06 21:51:55 -0500

    Add files via upload

---
+++ Facts.java
@@ -0,0 +1,14 @@
+// Tristan Goodell
+// contact@tgoodell.com
+// listing 2.2
+
+public class Facts {
+	public static void main (String[] args) {
+		System.out.println ("We present the following facts for your " + "extracurricular education:");
+		System.out.println ();
+		System.out.println ("Letters in the Hawaiian alphabet: 12");
+		System.out.println ("Dialing code for Antartica: " + 672);
+		System.out.println ("Year in which Leonardo da Vinci invented " + "the parachute: " + 1515);
+		System.out.println ("Speed of ketchup: " + 40 + " km per year");
+	}
+}
